2759a875ceebff0c12b5848f1102b0178c69f419
Watanabe Takashi <wtnb75@gmail.com>
2025-08-17 14:36:48 +0900

    sudo password

--- volexport/api.py
+++ volexport/api.py
@@ -13,22 +13,26 @@ api.include_router(volume_router)
 
 @api.exception_handler(FileNotFoundError)
 def notfound(request: Request, exc: FileNotFoundError):
+    _log.info("not found: request=%s %s", request.method, request.url.path, exc_info=exc)
     return JSONResponse(status_code=404, content=dict(detail="\n".join(exc.args)))
 
 
 @api.exception_handler(FileExistsError)
 def inuse(request: Request, exc: FileExistsError):
+    _log.info("file exists: request=%s %s", request.method, request.url.path, exc_info=exc)
     return JSONResponse(status_code=400, content=dict(detail="\n".join(exc.args)))
 
 
 @api.exception_handler(NotImplementedError)
 def notimplemented(request: Request, exc: NotImplementedError):
+    _log.info("not implemented: request=%s %s", request.method, request.url.path, exc_info=exc)
     return JSONResponse(status_code=501, content=dict(detail=str(exc)))
 
 
 @api.exception_handler(SubprocessError)
 def commanderror(request: Request, exc: SubprocessError):
-    return JSONResponse(status_code=500, content=dict(detail=str(exc)))
+    _log.info("command error: request=%s %s", request.method, request.url.path, exc_info=exc)
+    return JSONResponse(status_code=500, content=dict(detail="internal error"))
 
 
 @api.get("/health")
--- volexport/util.py
+++ volexport/util.py
@@ -13,7 +13,14 @@ def runcmd(cmd: list[str], root: bool = True):
             cmd = ["su", "-c", shlex.join(cmd)]
         elif config.BECOME_METHOD.lower() not in ("none", "false"):
             cmd[0:0] = shlex.split(config.BECOME_METHOD)
-    res = subprocess.run(cmd, capture_output=True, encoding="utf-8")
+    res = subprocess.run(
+        cmd,
+        capture_output=True,
+        encoding="utf-8",
+        timeout=config.CMD_TIMEOUT,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,
+    )
     _log.info("returncode=%s, stdout=%s, stderr=%s", res.returncode, repr(res.stdout), repr(res.stderr))
     res.check_returncode()
     return res
