0d100b57026055327e683385ce2652a1770073b1
Watanabe Takashi <wtnb75@gmail.com>
2025-08-30 00:02:23 +0900

    CLI: add compound operation (#13)
    * CLI: add compound operation
    
    * style
    
    * warning

--- volexport/client.py
+++ volexport/client.py
@@ -1,9 +1,11 @@
 import click
 import requests
 import functools
+from pathlib import Path
 from urllib.parse import urljoin, urlparse
 from logging import getLogger
 from .cli_utils import verbose_option, SizeType, output_format
+from .util import runcmd
 from .version import VERSION
 
 _log = getLogger(__name__)
@@ -329,6 +331,127 @@ def backup_delete(req, name):
     res = req.delete(f"/mgmt/backup/{name}")
     res.raise_for_status()
     return res.json()
+
+
+def iscsiadm(**kwargs):
+    arg = ["iscsiadm"]
+    for k, v in kwargs.items():
+        if len(k) == 1:
+            arg.append(f"-{k}")
+        else:
+            arg.append(f"--{k}")
+        if v is not None:
+            arg.append(v)
+    return runcmd(arg, root=True)
+
+
+def find_device(name, wait: int = 0):
+    import glob
+    import time
+
+    for _ in range(wait + 1):
+        for model in glob.glob("/sys/block/sd*/device/model"):
+            p = Path(model)
+            if p.read_text().strip() == name and (p.parent / "vendor").read_text().strip() == "VOLEXP":
+                devname = p.parent.parent.name
+                return f"/dev/{devname}"
+        else:
+            _log.info("wait and retry")
+            time.sleep(2)
+    return None
+
+
+@cli.command()
+@verbose_option
+@client_option
+@click.option("--name", required=True, help="volume name")
+@click.option("--format/--no-format", default=False, show_default=True)
+@click.option("--mount")
+def attach_volume(req: VERequest, name, format, mount):
+    """attach volume"""
+    import ifaddr
+
+    if format:
+        res = req.post(f"/volume/{name}/mkfs", json=dict(filesystem="ext4"))
+        res.raise_for_status()
+
+    addrs = []
+    for ad in ifaddr.get_adapters():
+        for ip in ad.ips:
+            if isinstance(ip.ip, tuple):
+                if ip.ip[2] != 0:
+                    # scope id (is link local address)
+                    continue
+                addr = ip.ip[0]
+            else:
+                addr = ip.ip
+            if addr:
+                addrs.append(addr)
+    res = req.post("/export", json={"volname": name, "acl": list(addrs)})
+    res.raise_for_status()
+    data = res.json()
+    addrs: list[str] = data["addresses"]
+    if addrs:
+        tgtaddr = addrs[0]
+    else:
+        _log.warning("volexp returns no ip address.")
+        tgtaddr = urlparse(req.baseurl).hostname
+        assert tgtaddr is not None
+    targetname: str = data["targetname"]
+    iscsiadm(m="discovery", t="st", p=tgtaddr)
+    iscsiadm(m="node", T=targetname, o="update", n="node.session.auth.authmethod", v="CHAP")
+    iscsiadm(m="node", T=targetname, o="update", n="node.session.auth.username", v=data["user"])
+    iscsiadm(m="node", T=targetname, o="update", n="node.session.auth.password", v=data["passwd"])
+    iscsiadm(m="node", T=targetname, l=None)
+    if mount:
+        devname = find_device(name, 10)
+        if devname is not None:
+            runcmd(["mount", devname, mount], root=True)
+        else:
+            raise Exception(f"volume not found: {name=}")
+    return
+
+
+@cli.command()
+@verbose_option
+@client_option
+@click.option("--name", help="volume name")
+def detach_volume(req: VERequest, name):
+    """detach volume"""
+
+    devname = find_device(name)
+    if devname is None:
+        raise FileNotFoundError(f"volume not attached: {name=}")
+
+    # find target
+    res = req.get("/export")
+    res.raise_for_status()
+    for tgt in res.json():
+        if tgt["volumes"] == [name]:
+            targetname = tgt["targetname"]
+            break
+    else:
+        raise FileNotFoundError(f"target not found: {name=}")
+
+    # umount if mounted
+    for line in Path("/proc/mounts").read_text().splitlines():
+        words = line.split()
+        if words[0] == devname:
+            _log.info("device %s mounted %s", words[0], words[1])
+            runcmd(["umount", words[0]], root=True)
+            break
+
+    res = iscsiadm(m="node", T=targetname, u=None)
+    portal = None
+    for line in res.stdout.splitlines():
+        if line.endswith("successful."):
+            portal = line.split("[", 1)[-1].split("]", 1)[0].rsplit(maxsplit=1)[-1]
+            break
+    if portal:
+        iscsiadm(m="discoverydb", t="st", p=portal, o="delete")
+
+    final_res = req.delete(f"/export/{targetname}", params={"force": "1"})
+    return final_res.json()
 
 
 if __name__ == "__main__":
---
+++ volexport/config2.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from typing_extensions import Annotated
+from pydantic import AfterValidator, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+HomePath = Annotated[Path, AfterValidator(lambda v: v.expanduser())]
+
+
+class ConfigServer(BaseSettings):
+    """Configuration settings for the volexport application."""
+
+    model_config = SettingsConfigDict(env_prefix="VOLEXP_", env_file=os.getenv("VOLEXP_ENV_FILE"))
+    VG: str = Field(description="Volume Group name")
+    NICS: list[str] = Field(description="List of network interfaces to use")
+
+
+config2 = ConfigServer()  # type: ignore
