fc42bb5813f34b2622f6b4e31a52eec1e5961659
Watanabe Takashi <wtnb75@gmail.com>
2025-09-22 17:32:53 +0900

    volexpcsi: add unittest (#23)

--- tests/test_csi_ident.py
+++ tests/test_csi_ident.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 from volexpcsi.identity import VolExpIdentity
 from volexpcsi import api
+from volexpcsi.server import boot_server
 
 
 class dummyctxt:
@@ -64,3 +65,11 @@ class TestCsiIdentity(unittest.TestCase):
         res = hdl.Probe(req, ctxt)
         self.assertFalse(res.ready.value)
         vreq.assert_called_once_with("/health")
+
+
+class TestCsiBoot(unittest.TestCase):
+    @patch("grpc.server")
+    def test_boot(self, server):
+        server.return_value.add_insecure_port.return_value = 9999
+        res = boot_server("localhost:9999", dict(endpoint="http://localhost:9998"))
+        self.assertEqual((9999, ANY), res)
