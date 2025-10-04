c1a58c8a04f952fb7ccb9f304e74179eee907aa2
Watanabe Takashi <wtnb75@gmail.com>
2025-10-04 22:41:49 +0900

    backup vg configurations

--- tests/test_mgmtapi.py
+++ tests/test_mgmtapi.py
@@ -1,5 +1,7 @@
 import unittest
 import tempfile
+import zipfile
+import io
 from pathlib import Path
 from unittest.mock import patch, ANY
 from fastapi.testclient import TestClient
@@ -30,7 +32,8 @@ hello world
         self.assertIn("name", res.json())
         name: str = res.json()["name"]
         self.assertTrue((Path(self.td.name) / name).with_suffix(".backup").exists())
-        run.assert_called_once_with(["sudo", "tgt-admin", "--dump"], **self.run_basearg)
+        run.assert_any_call(["sudo", "tgt-admin", "--dump"], **self.run_basearg)
+        run.assert_any_call(["sudo", "vgcfgbackup", "--file", ANY, "vg0"], **self.run_basearg)
 
         reslist = client.get("/mgmt/backup")
         self.assertEqual(200, reslist.status_code)
@@ -38,7 +41,9 @@ hello world
 
         resget = client.get(f"/mgmt/backup/{name}")
         self.assertEqual(200, resget.status_code)
-        self.assertEqual(run.return_value.stdout, resget.text)
+        zf = zipfile.ZipFile(io.BytesIO(resget.content))
+        exp = zf.read("export").decode("utf-8")
+        self.assertEqual(run.return_value.stdout, exp)
 
         resnotfound = client.get("/mgmt/backup/notfound")
         self.assertEqual(404, resnotfound.status_code)
@@ -59,15 +64,24 @@ hello world
         self.assertTrue((bkdir / "2025-09-01.backup").exists())
         self.assertTrue((bkdir / "2025-09-02.backup").exists())
 
+    def _example_backup(self, export: bytes, volume: bytes):
+        buf = io.BytesIO()
+        zf = zipfile.ZipFile(buf, "w")
+        zf.writestr("export", b"hello export\n")
+        zf.writestr("volume", b"hello volume\n")
+        zf.close()
+        return buf.getvalue()
+
     def test_put_delete(self):
         bkdir = Path(self.td.name)
-        res = TestClient(api).put("/mgmt/backup/2025-08-31", content=b"hello\n")
+        content = self._example_backup(b"hello export\n", b"hello volume\n")
+        res = TestClient(api).put("/mgmt/backup/2025-08-31", content=content)
         self.assertEqual(200, res.status_code)
         self.assertEqual({"status": "OK"}, res.json())
         self.assertTrue((bkdir / "2025-08-31.backup").exists())
-        self.assertEqual("hello\n", (bkdir / "2025-08-31.backup").read_text())
+        self.assertEqual(content, (bkdir / "2025-08-31.backup").read_bytes())
 
-        resexists = TestClient(api).put("/mgmt/backup/2025-08-31", content=b"hello\n")
+        resexists = TestClient(api).put("/mgmt/backup/2025-08-31", content=content)
         self.assertEqual(400, resexists.status_code)
         self.assertEqual({"detail": "backup already exists"}, resexists.json())
 
@@ -75,6 +89,31 @@ hello world
         self.assertEqual(200, resdel.status_code)
         self.assertEqual({"status": "OK"}, resdel.json())
         self.assertFalse((bkdir / "2025-08-31.backup").exists())
+
+    def test_put_invalid1(self):
+        content = self._example_backup(b"hello export\n", b"hello volume\n")
+        content = b"invalid" + content[7:]
+        res = TestClient(api).put("/mgmt/backup/2025-08-31", content=content)
+        self.assertEqual(400, res.status_code)
+        self.assertIn("detail", res.json())
+
+    def test_put_invalid2(self):
+        content = b"hello world\n"
+        res = TestClient(api).put("/mgmt/backup/2025-08-31", content=content)
+        self.assertEqual(400, res.status_code)
+        self.assertIn("detail", res.json())
+
+    def test_put_invalid3(self):
+        buf = io.BytesIO()
+        zf = zipfile.ZipFile(buf, "w")
+        zf.writestr("export", b"hello export\n")
+        zf.writestr("volume", b"hello volume\n")
+        zf.writestr("blabla", b"hello blabla\n")
+        zf.close()
+        content = buf.getvalue()
+        res = TestClient(api).put("/mgmt/backup/2025-08-31", content=content)
+        self.assertEqual(400, res.status_code)
+        self.assertIn("detail", res.json())
 
     def test_delete_notfound(self):
         res = TestClient(api).delete("/mgmt/backup/notfound")
@@ -84,11 +123,12 @@ hello world
     @patch("subprocess.run")
     def test_restore(self, run):
         bkdir = Path(self.td.name)
-        (bkdir / "2025-08-31.backup").write_text("123")
+        (bkdir / "2025-08-31.backup").write_bytes(self._example_backup(b"exp123", b"vol123"))
         res = TestClient(api).post("/mgmt/backup/2025-08-31")
         self.assertEqual(200, res.status_code)
         self.assertEqual({"status": "OK"}, res.json())
-        run.assert_called_once_with(["sudo", "tgt-admin", "-c", ANY, "-e"], **self.run_basearg)
+        run.assert_any_call(["sudo", "tgt-admin", "-c", ANY, "-e"], **self.run_basearg)
+        run.assert_any_call(["sudo", "vgcfgrestore", "--file", ANY, "vg0"], **self.run_basearg)
 
     def test_restore_notfound(self):
         res = TestClient(api).post("/mgmt/backup/notfound")
--- volexport/api_mgmt.py
+++ volexport/api_mgmt.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import FileResponse
 from pathlib import Path
+from typing import Iterator
+import io
 import datetime
+import zipfile
+import tempfile
 from .config import config
+from .config2 import config2
 from .tgtd import Tgtd
+from .lvm2 import VG
+from .exceptions import InvalidArgument
 from logging import getLogger
 
 _log = getLogger(__name__)
@@ -17,58 +24,79 @@ def _backup_file(name) -> Path:
     return (dir / name).with_suffix(".backup")
 
 
-@router.post("/mgmt/backup", description="create tgtd backup")
+def _list_backup() -> Iterator[Path]:
+    dir = Path(config.BACKUP_DIR)
+    return dir.glob("*.backup")
+
+
+@router.post("/mgmt/backup", description="create backup")
 def create_backup() -> dict[str, str]:
     basename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    outpath = _backup_file(basename)
-    outpath.write_text(Tgtd().dump())
-    _log.info("backup created: %s", outpath)
+    outfile = _backup_file(basename)
+    with zipfile.ZipFile(outfile, "w", compression=zipfile.ZIP_DEFLATED) as zf, tempfile.NamedTemporaryFile("r+") as tf:
+        zf.writestr("export", Tgtd().dump())
+        VG(config2.VG).backup(Path(tf.name))
+        zf.writestr("volume", Path(tf.name).read_bytes())
+    _log.info("backup created: %s", outfile)
     return {"name": basename}
 
 
-@router.get("/mgmt/backup", description="list tgtd backup files")
-def list_backup() -> list[dict[str, str]]:
-    dir = Path(config.BACKUP_DIR)
-    return [{"name": x.with_suffix("").name} for x in sorted(dir.glob("*.backup"))]
+@router.get("/mgmt/backup", description="list backup files")
+def list_backup() -> list[dict]:
+    return [dict(name=path.with_suffix("").name) for path in sorted(_list_backup())]
 
 
-@router.delete("/mgmt/backup", description="delete old tgtd backup file")
+@router.delete("/mgmt/backup", description="delete old backup file")
 def forget_backup(keep: int = 2) -> dict[str, str]:
-    dir = Path(config.BACKUP_DIR)
-    files = sorted(dir.glob("*.backup"), reverse=True)
+    files = sorted(_list_backup(), reverse=True)
     for f in files[keep:]:
-        _log.info("delete old backup: %s", f)
+        _log.info("delete old backup(export): %s", f)
         f.unlink()
     return {"status": "OK"}
 
 
-@router.get("/mgmt/backup/{name}", description="download tgtd backup file")
-def get_backup(name: str) -> PlainTextResponse:
+@router.get("/mgmt/backup/{name}", description="download backup file")
+def get_backup(name: str) -> FileResponse:
     path = _backup_file(name)
     if path.exists():
-        return PlainTextResponse(path.read_text())
+        return FileResponse(path, filename=path.name)
     raise FileNotFoundError("backup file not found")
 
 
-@router.put("/mgmt/backup/{name}", description="upload tgtd backup file")
+@router.put("/mgmt/backup/{name}", description="upload backup file")
 async def put_backup(name: str, req: Request) -> dict[str, str]:
     path = _backup_file(name)
     if path.exists():
         raise FileExistsError("backup already exists")
-    path.write_bytes(await req.body())
+    # validate
+    try:
+        buf = io.BytesIO(await req.body())
+        zf = zipfile.ZipFile(buf, "r")
+    except zipfile.BadZipFile as e:
+        raise InvalidArgument(f"invalid backup file: {e}")
+    chk = zf.testzip()
+    if chk is not None:
+        raise InvalidArgument(f"invalid backup file: {chk}")
+    if set(zf.namelist()) != {"volume", "export"}:
+        raise InvalidArgument("invalid backup file")
+
+    path.write_bytes(buf.getbuffer())
     return {"status": "OK"}
 
 
-@router.post("/mgmt/backup/{name}", description="restore tgtd backup")
+@router.post("/mgmt/backup/{name}", description="restore backup")
 def restore_backup(name: str) -> dict[str, str]:
     path = _backup_file(name)
     if path.exists():
-        Tgtd().restore(path.read_text())
+        with zipfile.ZipFile(path, "r") as zf, tempfile.NamedTemporaryFile("r+") as tf:
+            Tgtd().restore(zf.read("export").decode("utf-8"))
+            Path(tf.name).write_bytes(zf.read("volume"))
+            VG(config2.VG).restore(Path(tf.name))
         return {"status": "OK"}
     raise FileNotFoundError("backup file not found")
 
 
-@router.delete("/mgmt/backup/{name}", description="delete specified tgtd backup file")
+@router.delete("/mgmt/backup/{name}", description="delete specified backup file")
 def delete_backup(name: str) -> dict[str, str]:
     path = _backup_file(name)
     if path.exists():
